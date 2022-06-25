ad7aac82ae7483e5cd26c387d500a5f9893dc0b6
Arthur Nepomuceno <arth.nepomuceno@gmail.com>
2022-06-25 16:40:32 -0300

    feat: saving data on database

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import express from "express";
 import joi from "joi";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config()
+
 
 const server = express();
+const mongoClient = new MongoClient("mongodb://127.0.0.1:27017");
+let db;
+mongoClient.connect().then(() => {
+    db = mongoClient.db("uolDataBase")
+})
+
+
 server.use(express.json());
 
 server.post("/participants", (request, response) => {
@@ -19,12 +30,15 @@ server.post("/participants", (request, response) => {
     if(validation.error){
         response.status(422).send("Something went wrong :/")
     } else {
+        db.collection("participants").insert({body}) //saving participant on data base
         response.status(201).send(`Welcome, ${body.name}!`) 
     }
-
 })
 
 server.get("/participants", (request, response) => {
+    db.collection("participants").find().toArray().then((participant) => {
+        console.log(participant);
+    })
     response.send("ok")
 })
 
