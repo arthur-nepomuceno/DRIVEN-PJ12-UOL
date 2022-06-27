85dd7f1da8d5726acb08b438683687f28ff20c85
Arthur Nepomuceno <arth.nepomuceno@gmail.com>
2022-06-27 10:49:46 -0300

    Feat: post/messages - all set. Build: get participants messages

--- index.js
+++ index.js
@@ -63,30 +63,48 @@ server.get("/participants", async (request, response) => {
     }
 })
 
-// POST and GET messages
+
 server.post("/messages", async (request, response) => {
     const body = request.body;
-    const participantName = "Joaninha"; // request.header.user;
     const {to, text, type} = body;
+    const participantName = request.header.user;
     const time = dayjs().format('HH:mm:ss')
+    
     const participants = dbUol.collection("participants");
     const messages = dbUol.collection("messages");
     
-    // initiating validation
+    let isParticipantOnline;
     const participantsList = await participants.find({name: participantName}).toArray();
+    if(participantsList.length === 0){
+        isParticipantOnline = false;
+    } else {
+        isParticipantOnline = true;
+    }
+    
     const messageSchema = joi.object({
+        from: joi.string().required(),
         to: joi.string().required(),
-        text: joi.string().required()
-    })
+        text: joi.string().required(),
+        type: joi.any().valid("message", "private_message")
+    });
+
+    let isValidMessage;
+    const messageInfo = {from: participantName, to, text, type};
+    const validation = messageSchema.validate(messageInfo, {abortEarly: false})
+
+    if(validation.error || isParticipantOnline === false){
+        isValidMessage = false;
+    } else {
+        isValidMessage = true;
+    }
 
     try {
-        // after finishing validation schema, modify condition
-        if(participantsList.length > 0){
-            const newMessage = {from: participantName, to, text, type, time};
-            await messages.insertOne(newMessage);
+        if(isValidMessage){
+            const newMessage = {from: participantName, to, text, type, time}
+            await messages.insert(newMessage);
             response.status(201).send(`Message sent succeffuly!`)
         } else {
-            response.status(201).send(`Participant not on the list.`)
+            response.status(422).send(`Message not sent. Missing data.`)
         }
     } catch(error){
         response.status(500).send("Server error :(")
@@ -95,10 +113,25 @@ server.post("/messages", async (request, response) => {
 
 server.get("/messages", async (request, response) => {
     const messages = dbUol.collection("messages");
+    const limit = parseInt(request.query.limit);
+    const participantName = request.header.user;
     
     try{
-        const messagesList = await messages.find().toArray();
-        response.send(messagesList);
+        let messagesList = [];
+        const allMessages = await messages.find().toArray();
+        let lastMessagePosition = allMessages.length - 1;
+        
+        //Write routine for selecting participant available messages
+        
+        if(limit > lastMessagePosition || !limit){
+            response.send(allMessages);
+        } else if(limit < lastMessagePosition){
+            for(let i = 0; i < limit; i++){
+                const lastMessage = allMessages[lastMessagePosition - i];
+                messagesList.push(lastMessage);                
+            }
+            response.send(messagesList);
+        }
     } catch(error){
         response.status(500).send("Server error :(")
     }
